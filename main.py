4d64676decc0aef9b3ebff1ca73bfa98a4ae7a8e
truden-dev <torgeirar@gmail.com>
2024-04-03 12:54:35 +0200

    Adding pre-commit tags

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
+"""Main program"""
+
+import os
+from dataclasses import dataclass
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+@dataclass
+class Auth:
+    """Cognite auth parameters"""
+
+    tenant_id: str | None = os.getenv("TENANTID")
+    project: str | None = os.getenv("PROJECT")
+    base_url: str | None = os.getenv("BASEURL")
+    token: str | None = os.getenv("TOKEN")
+
+
 def main():
-    print("Hello World!")
+    """Main function"""
+    my_auth: Auth = Auth()
+    print(my_auth)
+
 
 if __name__ == "__main__":
     main()
